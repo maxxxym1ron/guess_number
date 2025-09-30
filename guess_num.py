e1d26bca3cf6b42fda452cb0fa9531b10a68001c
Maxim Kochnev <aakylkaa@yandex.ru>
2025-09-30 19:07:08 +0400

    Add difficult and attempts

--- guess_num.py
+++ guess_num.py
@@ -2,17 +2,57 @@ from random import randint
 
 a = randint(1, 100)
 
-print('\nУгадай число от 1 до 100\n')
+print('\nИГРА "Guess number 0-100"\n')
 
+print('Выберите сложность игры:')
+print('easy - 10 попыток угадать число')
+print('medium - 7 попыток угадать число')
+print('hard - 5 попыток угадать число')
+
+difficult = str(input('\nВведите сложность игры (easy/medium/hard): '))
+
+
+attempts = 10
 while True:
+    if difficult == 'easy':
+        print('\nВыбрана сложность easy.')
+        attempts = 10
+        break
+
+    elif difficult == 'medium':
+        print('\nВыбрана сложность medium.')
+        attempts = 7
+        break
+
+    elif difficult == 'hard':
+        print('\nВыбрана сложность hard.')
+        attempts = 5
+        break
+
+    else:
+        print('\nНекорректно введена сложность игры.\nПовторите попытку: ')
+        difficult = str(input())
+
+
+
+while attempts:
     guess = int(input('Введите число: '))
     
     if guess < a:
         print('\nВаше число меньше загаданного\n')
+        attempts -= 1
     
     elif guess > a:
         print('\nВаше число больше загаданного\n')
+        attempts -= 1
         
-    else:
-        print('\nВы отгадали число!\n')
+    elif guess == a:
+        print('\n-------------- ПОБЕДА -------------')
+        print(f'Вы отгадали число загаданное число: {a}\n')
         break
+
+    print(f'Осталось {attempts} попыток')
+
+if attempts == 0:
+    print('\n-------------- ПОРАЖЕНИЕ -------------')
+    print(f'Вы не отгадали загаданное число: {a}\n')
